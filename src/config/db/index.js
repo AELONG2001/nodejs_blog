29c99e2ed83effea9979b57ba2d513d456577403
long <longhk911@gmail.com>
2021-11-27 08:37:53 -0800

    load fix

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect() {
     try {
         await mongoose.connect(
-            'mongodb+srv://longdev2001:Longkmhd123.@cluster0.f3nvx.mongodb.net/f8_education_dev?retryWrites=true&w=majority' ||
+            process.env.MONGODB_URI ||
                 'mongodb://localhost:27017/f8_education_dev',
         );
         console.log('Connect successfully');
