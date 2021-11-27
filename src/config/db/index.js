e43ad4d49e5ce8fa3d8de7bd4ddc4bc429b00770
long <longhk911@gmail.com>
2021-11-26 20:54:13 -0800

    da

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -5,7 +5,7 @@ async function connect() {
         await mongoose.connect('mongodb://localhost:27017/f8_education_dev');
         console.log('Connect successfully');
     } catch (error) {
-        console.log('Connect failure');
+        console.log('Connect failure hz');
     }
 }
 
