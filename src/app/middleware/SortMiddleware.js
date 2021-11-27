9767488f360eb8da2db91a0c235d437b9a214997
long <longhk911@gmail.com>
2021-11-26 20:20:44 -0800

    dd

---
+++ src/app/middleware/SortMiddleware.js
@@ -0,0 +1,19 @@
+module.exports = function sortMiddleware(req, res, next) {
+    res.locals._sort = {
+        enabled: false,
+        type: 'default',
+    };
+
+    if (req.query.hasOwnProperty('_sort')) {
+        Object.assign(res.locals._sort, {
+            enabled: true,
+            type: req.query.type,
+            column: req.query.column,
+        });
+
+        //Object.assign là để hợp nhất object từ phải => trái
+        // ==> Nếu mà trùng key thì ( key của object bên phải sẽ ghi đè key của object bên trái)
+    }
+
+    next();
+};
