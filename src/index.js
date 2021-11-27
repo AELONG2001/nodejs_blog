b8d83b00848dece7af4f1363ca25c9a30bbe4a5e
long <longhk911@gmail.com>
2021-11-26 20:26:50 -0800

    dd

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ app.use(logger('dev'));
 //Template engine
 const hbs = handlebars.create({
     extname: '.hbs',
-    helpers: require('./helpers/handlebars'),
+    helpers: {
+        sortable: (field, sort) => {
+            const sortType = field === sort.column ? sort.type : 'default';
+
+            const icons = {
+                default: 'bx bxs-sort-alt',
+                asc: 'bx bx-sort-up',
+                desc: 'bx bx-sort-down',
+            };
+
+            const icon = icons[sortType];
+
+            //=====
+
+            const types = {
+                default: 'desc',
+                asc: 'desc',
+                desc: 'asc',
+            };
+
+            const type = types[sortType];
+
+            const address = Handlebars.escapeExpression(
+                `?_sort&column=${field}&type=${type}`,
+            );
+
+            const output = `
+                <a href="${address}" class="text-decoration-none">
+                <i class="${icon}" style="transform: translateY(2px)"></i>
+                </a>
+            `;
+
+            return new Handlebars.SafeString(output);
+        },
+    },
 });
 
 app.engine('.hbs', hbs.engine);
